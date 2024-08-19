8b261563e6ca692388a881f70455fd499a9ef9ed
DhanushKanagaraj <166890612+DhanushKanagaraj@users.noreply.github.com>
2024-08-19 21:25:45 +0530

    page initiate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+  return(
+    <>
+    <div className="text-lg">Hello World</div>
+   <div className="border-2 bg-gray-100"> Home</div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        redf:"#7C7C7C"
+      }
+    },
+  },
+  plugins: [],
+}
