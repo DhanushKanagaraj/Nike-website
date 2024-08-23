d2f7641851b88b8d0b6b15c00c568f6e46614cad
DhanushKanagaraj <166890612+DhanushKanagaraj@users.noreply.github.com>
2024-08-23 23:46:20 +0530

    shoe animation

--- src/componenets/Shoecard.jsx
+++ src/componenets/Shoecard.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-const Shoecard = () => {
+const Shoecard = ({ imgURL, changeBigShoeImage, bigShoeImage }) => {
+
+  const handleClick = () => {
+    if (bigShoeImage !== imgURL) {
+      changeBigShoeImage(imgURL);
+    }
+  };
+
   return (
-    <div>Shoecard</div>
-  )
-}
+    <div
+      className={`border-2 rounded-xl ${bigShoeImage === imgURL ? 'border-coral-red' : 'border-transparent'} cursor-pointer max-sm:flex-1`}
+      onClick={handleClick}
+    >
+      <div className='flex justify-center items-center bg-card bg-center sm:w-40 sm:h-40 rounded-xl mx-sm:p-4'>
+        <img
+          src={imgURL.thumbnail}
+          alt="shoe-thumbnail"
+          width={127}
+          height={103}
+
+          className='object-contain'
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Shoecard
